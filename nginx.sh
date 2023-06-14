2001d6f55a8e6921d7e51bdaf9a5e121a7100f12
jbgomezr <38460829+jbgomezr@users.noreply.github.com>
2023-06-14 20:41:40 +0200

    Create nginx.sh

---
+++ nginx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Actualizar paquetes del sistema
+sudo apt-get update
+
+# Instalar Nginx
+sudo apt-get install -y nginx
+
+# Iniciar servicio de Nginx
+
+sudo systemctl start nginx
